905127f4c7c5d6164f4677ae921919c175261275
naeramit <naeramit.18@gmail.com>
2023-09-20 16:45:27 +0700

    fix bug

--- src/routes/room-route.js
+++ src/routes/room-route.js
@@ -8,7 +8,7 @@ router.get("/", roomController.getData);
 router.post("/student/enter", roomController.studentEnter);
 router.post("/ta/enter", roomController.taEnter);
 router.delete("/ta/logout", roomController.taLogOut);
-router.delete("/student/logout", roomController.taLogOut);
+router.delete("/student/logout", roomController.studentLogOut);
 router.post("/sos/:userId", roomController.createSOS);
 router.delete("/sos/:userId", roomController.cancelSOS);
 router.post("/remark/:userId", roomController.creteRemark);
